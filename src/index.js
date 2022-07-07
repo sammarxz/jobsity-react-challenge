813ff8d89b97bbb2f70ee66127fe3d422b6a984c
Sam Marxz <sammarxzz@gmail.com>
2022-07-07 11:41:30 -0300

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -10,26 +10,6 @@ import reducers from "./store/reducers";
 
 import "./styles/index.css";
 
-// createServer({
-//   routes() {
-//     this.get("/api/reminders", () => ({
-//       reminders: [
-//         { id: 1, text: "Walk the dog" },
-//         { id: 2, text: "Take out the trash" },
-//         { id: 3, text: "Work out" },
-//       ],
-//     }));
-
-//     let newId = 4;
-//     this.post("/api/reminders", (schema, request) => {
-//       let attrs = JSON.parse(request.requestBody);
-//       attrs.id = newId++;
-
-//       return { reminder: attrs };
-//     });
-//   },
-// });
-
 makeServer();
 
 ReactDOM.render(
